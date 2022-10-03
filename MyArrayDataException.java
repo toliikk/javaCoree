be3403e6afe6f521103a1661f2e7846e155f4229
toliikk <114696772+toliikk@users.noreply.github.com>
2022-10-03 21:41:34 +0300

    Add files via upload

---
+++ MyArrayDataException.java
@@ -0,0 +1,10 @@
+package leesone11;
+
+public class MyArrayDataException extends Throwable {
+    public String i;
+    public String j;
+
+    public MyArrayDataException(int i, int j) {
+
+    }
+}
---
+++ MyArraySizeException.java
@@ -0,0 +1,4 @@
+package leesone11;
+
+public class MyArraySizeException extends Throwable {
+}
